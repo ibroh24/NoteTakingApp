9122a79102fb9de01951ef113513db7d3aef52c3
Ibrahim Hammed <ibroh24@gmail.com>
2021-07-14 18:23:58 +0000

    node restAPI depencies initialization

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+// start creating app with express
+const app = express();
+
+// parse requests of content-type
+app.use(bodyParser.urlencoded({extended:true}));
+
+// parse requests of content-type: app/json
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+// connect to database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser:true
+}).then(()=>{
+    console.log('connected to db');
+}).catch(err=>{
+    console.log('could not connect to the db', err);
+    process.exit();
+});
+
+
+// a simple route
+app.get('/', (req, res) =>{
+    res.json({
+        "message": "Welcome to Node RestAPI"
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log("server listening on port 3000...");
+});
